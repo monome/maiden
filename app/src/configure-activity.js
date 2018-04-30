b7d42a7db421eca75a6dd25254d865dd619b782c
Greg Wuller <greg@afofo.com>
2018-04-29 23:40:20 -0700

    display version in configure activity

--- app/src/configure-activity.js
+++ app/src/configure-activity.js
@@ -1,9 +1,24 @@
 // import React, { Component } from 'react';
 import React from 'react';
+import { VERSION, COMMIT } from './version';
+
+import './configure-activity.css';
 
 const ConfigureActivity = (props) => {
+    let style = {
+        height: props.height,
+        width: props.width,
+    };
+
     return (
-        <div className="configure-activity">config</div>
+        <div className="configure-activity" style={style}>
+            <div className="configure-about">
+                <span>version: {VERSION} ({COMMIT})</span>
+            </div>
+            <div className="configure-tbd">
+                <span>tbd...</span>
+            </div>
+        </div>
     );
 };
 
