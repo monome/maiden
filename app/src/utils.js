d0037c246a3520e54097f584d8525d94d6cd03d6
pq <pquitslund@google.com>
2018-05-07 16:32:21 -0700

    fixed ctrl

--- app/src/utils.js
+++ app/src/utils.js
@@ -4,7 +4,7 @@ function OS() {
 OS.isMac = navigator.appVersion.indexOf('Mac') !== -1;
 
 OS.metaKey = function metaKey() {
-  return OS.isMac ? '⌘' : 'ctrl';
+  return OS.isMac ? '⌘' : 'ctrl+';
 };
 
 export default OS;
