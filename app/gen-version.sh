9fd6f78ae76f0e0d850b13469acead59e401f67b
Greg Wuller <greg@afofo.com>
2018-05-26 21:48:12 -0700

    make gen-version emit single qoutes

--- app/gen-version.sh
+++ app/gen-version.sh
@@ -4,6 +4,6 @@ version=$(git describe --tags | cut -c 2- | cut -d '-' -f 1)
 commit=$(git describe --always --dirty)
 
 cat > src/version.js << EOF
-export const VERSION = "${version}";
-export const COMMIT = "${commit}";
+export const VERSION = '${version}';
+export const COMMIT = '${commit}';
 EOF
