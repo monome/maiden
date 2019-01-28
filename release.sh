80dbd4f46850a31f74552814540d80d130fe2bb5
Greg Wuller <greg@afofo.com>
2019-01-27 19:32:16 -0800

    initial pass at build release script

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# quick and dirty release script
+REL_DIR=./dist/maiden
+
+rm -rf ./dist
+mkdir -pv $REL_DIR
+
+# maiden
+echo -e "building maiden"
+echo "====================="
+cmd='GOOS=linux GOARCH=arm go build -o maiden.arm'
+echo $cmd
+eval $cmd
+cp -v maiden.arm $REL_DIR
+cp -v start.sh $REL_DIR
+
+# app
+echo -e "\nbuilding app"
+echo "====================="
+cmd='(cd app; yarn build)'
+echo $cmd
+eval $cmd
+mkdir -pv ${REL_DIR}/app
+cp -rv ./app/build ${REL_DIR}/app
+
+# tarball
+echo -e "\nmaking release"
+echo "====================="
+cmd='(cd ./dist; tar czvf maiden.tgz maiden)'
+echo $cmd
+eval $cmd
