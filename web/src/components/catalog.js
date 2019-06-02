d583fbcacf378c3e161913e5b88bf89f19dc3677
Greg Wuller <greg@afofo.com>
2019-06-01 22:00:28 -0700

    initial pass at catalog component

---
+++ web/src/components/catalog.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import TextButton from './text-button';
+import ProjectInfo from './project-info';
+import ProjectControl from './project-control';
+
+import './catalog.css';
+
+const Catalog = (props) => {
+  const {catalog} = props;
+  const catalogURL = catalog.get('url');
+
+  const entries = catalog.get('entries').map(e => {
+    const projectName = e.get('project_name');
+    const projectURL = e.get('project_url');
+    return (
+      <li>
+      <ProjectControl key={projectURL}>
+        <ProjectInfo project={e} />
+        <TextButton color="hsl(0, 0%, 59%)"
+          action={() => props.installAction(catalogURL, projectName)}
+        >
+          install  
+        </TextButton>
+      </ProjectControl>
+      </li>
+    );
+  });
+
+  return (
+    <div className='catalog-container'>
+      <div className='catalog-name'>
+        {catalog.get('name')}
+      </div>
+      <ul className='catalog-entries'>
+        {entries}
+      </ul>
+    </div>
+  );
+}
+
+export default Catalog;
---
+++ web/stories/catalog.stories.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import { fromJS } from 'immutable';
+
+import Catalog from '../src/components/catalog';
+
+const mockLinesCatalog = fromJS({
+  "entries": [
+      {
+          "author": "Lee Azzarello (lazzarello)",
+          "description": "foo bar baz",
+          "origin": "lines",
+          "project_name": "fm7",
+          "project_url": "https://github.com/lazzarello/fm7",
+          "tags": [
+            "one",
+          ],
+      },
+      {
+          "author": "mark john williamson (junklight)",
+          "description": "",
+          "origin": "lines",
+          "project_name": "kria_midi",
+          "project_url": "https://github.com/junklight/misc"
+      },
+      {
+          "author": "jai lai bookie (Dan_Derks)",
+          "description": "",
+          "origin": "lines",
+          "project_name": "less_concepts",
+          "project_url": "https://github.com/dndrks/less-concepts-norns"
+      },
+      {
+          "author": "its your bedtime (its_your_bedtime)",
+          "description": "",
+          "origin": "lines",
+          "project_name": "orca",
+          "project_url": "https://github.com/hundredrabbits/Orca#operators"
+      },
+  ],
+  "name": "lines",
+  "updated": "0001-01-01T00:00:00Z",
+  "url": "/api/v1/catalog/lines",
+});
+
+storiesOf('Catalog', module)
+  .add('single catalog', () => (
+    <Catalog catalog={mockLinesCatalog}
+      installAction={(url, name) => {
+        action('do install', url, name);
+      }}
+    />
+  ));
