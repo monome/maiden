52426017cc8dc6d6552c63da567f1d083bbc9b22
Greg Wuller <greg@afofo.com>
2019-07-05 10:36:25 -0700

    remove debug print

--- web/src/model/edit-helpers.js
+++ web/src/model/edit-helpers.js
@@ -1,7 +1,5 @@
 export const bufferIsEditable = (buffer) => {
   const contentType = buffer.get('contentType');
-  const editable = contentType.includes('text') || contentType.includes('application/json');
-  console.log('buffer editable: ', editable, contentType);
-  return editable;
+  return contentType.includes('text') || contentType.includes('application/json');
 };
 
