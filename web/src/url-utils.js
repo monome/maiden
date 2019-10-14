a3fe6336ef0e6a3334fcb56e6729da03308d7b50
Ryan Laws <ryanlaws@gmail.com>
2019-10-13 22:59:13 -0700

    Fix history and flashing directory

--- web/src/url-utils.js
+++ web/src/url-utils.js
@@ -19,5 +19,15 @@ export const pathToResource = (path) => {
 }
 
 export const isEditPath = (path) => {
-    return path && path.startsWith(EDIT_PREFIX)
+  return path && path.startsWith(EDIT_PREFIX)
+}
+
+export const inSameDir = (file1, file2) => {
+  if (!file1 || !file2) {
+    return false;
+  }
+
+  const getDir = file => file.split('/').slice(0, -1).join('/');
+  const [dir1, dir2] = [file1, file2].map(getDir);
+  return dir1 === dir2;
 }
